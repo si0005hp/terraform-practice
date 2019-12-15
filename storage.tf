ef57f0b50b7443089c19ac243f80a498b5193a91
si0005hp <si0005hp@gmail.com>
2019-12-15 20:12:37 +0900

    init

---
+++ storage.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "private" {
+  bucket = "terraform-sample-private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
